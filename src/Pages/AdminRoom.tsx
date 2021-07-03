29455450a16fca9d8fe52aacd96e4f4e4a2674af
mauricionofre <mauricionofre@hotmail.com>
2021-07-02 21:23:03 -0300

    aula 5

--- src/Pages/AdminRoom.tsx
+++ src/Pages/AdminRoom.tsx
@@ -2,6 +2,9 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import checkImg from '../assets/images/check.svg';
+import answerImg from '../assets/images/answer.svg';
+
 import { database } from '../services/firebase';
 
 import { Button } from '../components/Button';
@@ -19,20 +22,33 @@ export function AdminRoom() {
   const history = useHistory();
   const params = useParams<RoomParams>();
   const roomId = params.id;
+
   const { questions, title } = useRoom(roomId);
+  console.log(questions)
+  async function handleEndRoom() {
+    database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date(),
+    })
 
-async function handleEndRoom() {
-  database.ref(`rooms/${roomId}`).update({
-    endedAt: new Date(),
-  })
-
-  history.push('/');
-}
+    history.push('/');
+  }
 
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
       await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
+  }
+
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlighQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighlighted: true,
+    });
   }
 
   return (
@@ -44,7 +60,7 @@ async function handleEndRoom() {
             <RoomCode code={roomId} />
             <Button isOutlined
               onClick={handleEndRoom}
-              >Encerrar sala</Button>
+            >Encerrar sala</Button>
           </div>
         </div>
       </header>
@@ -56,7 +72,7 @@ async function handleEndRoom() {
             questions.length > 0 && <span>{questions.length} pergunta(s)</span>
           }
         </div>
-        
+
         <div className="question-list">
           {questions.map(question => {
             return (
@@ -64,7 +80,27 @@ async function handleEndRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
+                isAnswered={question.isAnswered}
+                isHighlighted={question.isHighlighted}
               >
+                {!question.isAnswered && (
+                  <>
+                    <button
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+
+                    <button
+                      type="button"
+                      onClick={() => handleHighlighQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Dar destaque para pegunta" />
+                    </button>
+                  </>
+                )}
+
                 <button
                   type="button"
                   onClick={() => handleDeleteQuestion(question.id)}
@@ -80,3 +116,5 @@ async function handleEndRoom() {
     </div>
   )
 }
+
+
